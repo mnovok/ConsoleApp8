9e8156d2a99a5d42b7ae5512fdb56ba6394392cb
ninarada <92435431+ninarada@users.noreply.github.com>
2023-03-16 16:28:43 +0100

    ispravljeno

--- NewClass.cs
+++ NewClass.cs
@@ -8,13 +8,11 @@ namespace ConsoleApp8
 {
     class NewClass
     {
-<<<<<<< HEAD
+
        static void Main(string[] args)
         {
 
         }
-=======
-        //hi
->>>>>>> 14bedb44e1e96492c2bdf8827664a18bf9c57ffa
+        //ispravljeno
     }
 }
